febddfb9399a4462b3205990ba7dc049bc4197b4
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-29 02:50:13 +0800

    implemented it to app

--- calendar-task-scheduler/src/App.jsx
+++ calendar-task-scheduler/src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+import dayjs from "dayjs";
 import Header from "./components/Header";
 
 const App = () => {
+  const [currentMonth, setCurrentMonth] = useState(dayjs());
+
   return (
     <div className="p-6 max-w-4xl mx-auto">
       <h1 className="text-2xl font-bold mb-4">React Calendar Task Scheduler</h1>
-      <Header />
+      <Header currentMonth={currentMonth} setCurrentMonth={setCurrentMonth} />
     </div>
   );
 };
