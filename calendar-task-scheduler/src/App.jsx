7970896e2d1264038658be6e3019066f9773f931
Joebeck Andrew F. Gusi <joebeckandrewfgusi@iskolarngbayan.pup.edu.ph>
2025-07-29 02:43:17 +0800

    test

--- calendar-task-scheduler/src/App.jsx
+++ calendar-task-scheduler/src/App.jsx
@@ -1,10 +1,9 @@
-
-function App() {
-  
+const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div className="p-6 max-w-4xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Calendar Task Scheduler</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
